84a28bee07bb867ea13dc83067fccd197791520c
mendi eisenbach <menachemaiz.gmail.com>
2025-06-26 20:27:44 +0300

    Remove node_modules from repo

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
-import readline from 'readline-sync';
+// import readline from 'readline-sync';
 
-const name = readline.question('איך קוראים לך? ');
-console.log(`שלום, ${name}!`);
+// const name = readline.question('? ');
+// console.log(`hallo, ${name}!`);
+
+import PromptSync from 'prompt-sync';
+import Player from './classes/Player.js';
+import Riddle from './classes/Riddle.js';
+import AllRiddles from './riddles/AllRiddles.js';
+
+const prompt = PromptSync();
+
+console.log("Welcome to the trivia quiz!")
+
+const name = prompt("Please enter your name: ")
+
+const myplayer = new Player(name)
+
+for(let oneriddle of AllRiddles)
+{
+    const startTime = Date.now();
+
+    const myriddle = new Riddle(oneriddle.id,
+                                oneriddle.name,
+                                oneriddle.taskDescription,
+                                oneriddle.correctAnswer);
+
+    myriddle.ask(prompt)
+    const endTime = Date.now();
+
+    myplayer.recordTime(startTime, endTime)
+}
+
+console.log(`\n${myplayer.name} You answered all the questions correctly!`);
+myplayer.showStats()
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,4 @@
+import Player from './classes/Player.js';
+
+const p = new Player('Tester');
+console.log(p.name);
