2c2f68c0d44e1aeefc00ec5ba2ee2c8686af3ed6
mendi eisenbach <menachemaiz.gmail.com>
2025-07-21 10:31:58 +0300

    Adding a SUPABASE db for saving players and making adjustments.

--- client/app.js
+++ client/app.js
@@ -57,40 +57,21 @@ const name = prompt("Please enter your name: ")
 const myplayer = new Player(name)
 
 let players = [];
-  try {
-    players = await fetch(`${serverUrl}/api/players`)
-      .then(res => res.json());
-  } catch (err) {
-    console.error("Error fetching players:", err);
-  }
-  const existingPlayer = players.find(p => p.name.toLowerCase() === name.toLowerCase());
-
-  if (existingPlayer) {
-    console.log(`Welcome back, ${name}! Your best time is ${existingPlayer.lowestTime} seconds.`);
-  } else {
-    console.log(`Welcome, ${name}! This is your first time playing.`);
-  }
+try {
+  const response = await fetch(`${serverUrl}/api/players`);
+  const json = await response.json();
+  players = json.data || json; 
+} catch (err) {
+  console.error("Error fetching players:", err);
+}
+const existingPlayer = players.find(p => p.name.toLowerCase() === name.toLowerCase());
 
 
-
-
-// let players = [];
-// try {
-//   const response = await fetch(`${serverUrl}/api/players`);
-//   const json = await response.json();
-//   players = json.data || json; 
-// } catch (err) {
-//   console.error("Error fetching players:", err);
-// }
-// console.log("Players received from server:", players);
-// const existingPlayer = players.find(p => p.name.toLowerCase() === name.toLowerCase());
-
-
-// if (existingPlayer) {
-//   console.log(`Welcome back, ${name}! Your best time is ${existingPlayer.lowestTime} seconds.`);
-// } else {
-//   console.log(`Welcome, ${name}! This is your first time playing.`);
-// }
+if (existingPlayer) {
+  console.log(`Welcome back, ${name}! Your best time is ${existingPlayer.lowestTime} seconds.`);
+} else {
+  console.log(`Welcome, ${name}! This is your first time playing.`);
+}
 
 
 
