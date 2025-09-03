d3c45cd9fc1420149e658da88fa1b92475623559
mendi eisenbach <menachemaiz.gmail.com>
2025-09-03 17:08:38 +0300

    qdd cors

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import riddlesRoutes from './routes/riddlesRoutes.js';
 import playersRoutes from './routes/playersRoutes.js';
 import { connectToMongo } from './riddles/mongoService.js';
+import cors from 'cors';
 
 
 const server = express();
 const port = 4545;
-
+app.use(cors()); 
+app.get('/test', (req, res) => {
+  res.json({ message: 'CORS fonctionne !' });
+});
 server.use(express.json());
 
 server.use('/api/riddles', riddlesRoutes);
